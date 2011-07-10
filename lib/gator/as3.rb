5417896735c8ccdc631505014501be877c11ab35
devboy <dominic.graefen@gmail.com>
2011-07-10 19:02:55 +0200

    Lets move this into a generators directory.

--- lib/gator/as3.rb
+++
@@ -1,3 +0,0 @@
-require File.dirname(__FILE__) + '/as3/collection'
-
-Gator::GenerateCommand.register_subcommand Gator::AS3::GeneratorCollection
---
+++ lib/gator/as3/generators.rb
@@ -0,0 +1,3 @@
+require File.dirname(__FILE__) + '/generators/collection'
+
+Gator::GenerateCommand.register_subcommand Gator::AS3::GeneratorCollection
