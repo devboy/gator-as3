a8b77366bd388df124381ec3765dc25c7035ca4a
devboy <dominic.graefen@gmail.com>
2011-08-11 15:45:34 +0200

    Added tests.

--- spec/generators/collection_spec.rb
+++ spec/generators/collection_spec.rb
@@ -3,3 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Gator::AS3::GeneratorCollection do
   it "should have some tests"
 end
+
+describe "fakeTest" do
+  it "fake" do
+    1.should == 1
+  end
+end
