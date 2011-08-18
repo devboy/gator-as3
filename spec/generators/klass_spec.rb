5d518a122216cc89fe47140564d901d675ac693d
devboy <dominic.graefen@gmail.com>
2011-08-18 22:52:39 +0200

    TestEnvironment

--- spec/generators/klass_spec.rb
+++ spec/generators/klass_spec.rb
@@ -1,5 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+class GatorProcess
+  def run
+    fork do
+      Dir.chdir "/Users/devboy/Development/Source/Ruby/gator-as3-testproject"
+      require "gator"
+      Gator::Util.initialize_files
+    end
+  end
+end
+
+describe "gator" do
+  it "should run in a different thread" do
+    gator = GatorProcess.new
+
+    gator.should_receive(:fork) do |&block|
+      block.call
+      Gator::Runner.resolve_subcommand("generate").should == Gator::GenerateCommand
+    end
+
+    gator.run
+  end
+end
+
 describe Gator::AS3::ClassGenerator do
   it "should have some tests"
 end
