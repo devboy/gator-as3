acd2530db63e3c65b0e1e4abc60c224754fbbeb8
Simon Bailey <simon@newtriks.com>
2011-08-18 15:04:38 +0100

    Added Test Runner class generator and template.
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- lib/gator/as3/generators/test/asunit4/runner.rb
+++ lib/gator/as3/generators/test/asunit4/runner.rb
@@ -1,24 +1,27 @@
 module Gator
   module AS3
-    module RobotLegs
-      module ASUnit4
+    module ASUnit4
 
-        class RunnerGenerator < Gator::AS3::AS3Generator
-          include Gator::Project
+      class RunnerGenerator < Gator::AS3::AS3Generator
+        include Gator::Project
 
-          define :command => "runner",
-                 :usage => "generate as3 test runner CLASS_NAME", :description => "Creates ASUnit4 suite."
-          
-          def template_file
-            "as3/test/asunit4/runner.as.tt"
-          end
+        define :command => "runner",
+               :usage => "generate as3 asunit4 test runner CLASS_NAME", :description => "Creates ASUnit4 runner."
 
-          def generate
-            @class_name += "Runner"
-            super
-          end
-
+        def source
+          project.path(:source, :test, :as3)
         end
+
+        def template_file
+          "as3/test/asunit4/runner.as.tt"
+        end
+
+        def generate
+          @package_name = ""
+          @class_name = "ASUnit4Runner"
+          super
+        end
+
       end
     end
   end
