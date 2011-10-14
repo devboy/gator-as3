0388de221ad609509843ea333d2d6c5ef1f08a42
devboy <dominic.graefen@gmail.com>
2011-10-14 19:36:32 +0200

    Removes older generator file.

--- lib/gator/mxml/generators.rb
+++
@@ -1,5 +0,0 @@
-require File.dirname(__FILE__) + '/../as3/utils/as3_util'
-require File.dirname(__FILE__) + '/generators/generator'
-require File.dirname(__FILE__) + '/generators/collection'
-
-Gator::GenerateCommand.register_subcommand Gator::MXML::GeneratorCollection
